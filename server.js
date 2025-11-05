0cc72397c950fec030ec9f764ff51eb9754b6657
mrshah63 <bsit4thse@gmail.com>
2025-11-05 14:31:51 +0000

    Fix static file serving for public directory

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(session({
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
 
 // Set view engine
 app.set('view engine', 'ejs');
